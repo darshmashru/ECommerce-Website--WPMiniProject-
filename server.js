d489f85051fd1de6c2166fc9ddf41a299ff2563c
Darsh Mashru <mashrudarsh31@gmail.com>
2023-04-06 12:00:17 +0530

    new updates

--- server.js
+++ server.js
@@ -1,72 +1,7 @@
-// import { connect } from '@planetscale/database'
-// import express from 'express';
-// import ejs from 'ejs';
-// import fs from 'fs/promises';
-
-// // Config for PlanetScale
-// const config = {
-//   host: process.env.PLANETSCALE_DB_HOST,
-//   username: process.env.PLANETSCALE_DB_USERNAME,
-//   password: process.env.PLANETSCALE_DB_PASSWORD
-// }
-
-// const conn = connect(config)
-
-// let html = ''; // Global variable to store the HTML contents
-// let results = null; // Global variable to store the database results
-
-// // Read the contents of the HTML file and execute database query when the server starts up
-// Promise.all([fs.readFile('candles.html', 'utf8'), conn.execute('SELECT * from merkaba.candles where id="1"')])
-//   .then(([file, res]) => {
-//     html = file;
-//     results = res;
-//   })
-//   .catch((err) => {
-//     console.error('Error occurred while initializing server:', err);
-//     process.exit(1);
-//   });
-
-// const app = express();
-
-// app.get('/candles.html', (req, res) => {
-//   if (results === null) {
-//     res.status(500).send('Internal server error. Please try again later.');
-//     return;
-//   }
-
-//   // Replace the corresponding text in the HTML file using ejs
-//   const data = {
-//     c_id: results.rows[0].id,
-//     c_name: results.rows[0].name,
-//     c_description: results.rows[0].description,
-//     c_price: results.rows[0].price,
-//     c_stock: results.rows[0].stock,
-//     c_category: results.rows[0].category
-//   };
-//   const renderedHtml = ejs.render(html, data);
-//   res.send(renderedHtml);
-// });
-
-// app.use(express.static('.'));
-
-// app.listen(3000, () => {
-//   console.log('Server started on port 3000');
-// });
-
-// export default app;
-
-
-// new logic
-
 import { connect } from '@planetscale/database'
 import express from 'express';
 import ejs from 'ejs';
 import fs from 'fs/promises';
-
-import { fileURLToPath } from 'url';
-import path from 'path';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // Config for PlanetScale
 const config = {
@@ -81,7 +16,7 @@ let html = ''; // Global variable to store the HTML contents
 let results = null; // Global variable to store the database results
 
 // Read the contents of the HTML file and execute database query when the server starts up
-Promise.all([fs.readFile(path.join(__dirname, 'candles.html'), 'utf8'), conn.execute('SELECT * from merkaba.candles where id="1"')])
+Promise.all([fs.readFile('./candles.html', 'utf8'), conn.execute('SELECT * from merkaba.candles where id="1"')])
   .then(([file, res]) => {
     html = file;
     results = res;
@@ -112,17 +47,8 @@ app.get('/candles.html', (req, res) => {
   res.send(renderedHtml);
 });
 
-// app.use(express.static('.'));
-app.use(express.static(path.join(__dirname, '/')));
+app.use(express.static('.'));
 
-
-// app.listen(3000, () => {
-//   console.log('Server started on port 3000');
-// });
-
-const PORT = process.env.PORT || 80;
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
 });
-
-export default app;
