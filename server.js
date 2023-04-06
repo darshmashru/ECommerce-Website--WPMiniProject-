a23a0e04c56e128a4cc9538a85f0d4f24694e2c6
Darsh Mashru <mashrudarsh31@gmail.com>
2023-04-06 12:49:20 +0530

    Connected Necklaces to Database

--- server.js
+++ server.js
@@ -81,7 +81,20 @@ let html = ''; // Global variable to store the HTML contents
 let results = null; // Global variable to store the database results
 
 // Read the contents of the HTML file and execute database query when the server starts up
-Promise.all([fs.readFile(path.join(__dirname, 'candles.html'), 'utf8'), conn.execute('SELECT * from merkaba.candles where id="1"')])
+
+// Candles
+// Promise.all([fs.readFile(path.join(__dirname, 'candles.html'), 'utf8'), conn.execute('SELECT * from merkaba.candles where id="1"')])
+//   .then(([file, res]) => {
+//     html = file;
+//     results = res;
+//   })
+//   .catch((err) => {
+//     console.error('Error occurred while initializing server:', err);
+//     process.exit(1);
+//   });
+
+// Index
+Promise.all([fs.readFile(path.join(__dirname, 'index.html'), 'utf8'), conn.execute('SELECT * FROM merkaba.necklaces;')])
   .then(([file, res]) => {
     html = file;
     results = res;
@@ -112,6 +125,28 @@ app.get('/candles.html', (req, res) => {
   res.send(renderedHtml);
 });
 
+app.get('/', (req, res) => {
+  if (results === null) {
+    res.status(500).send('Internal server error. Please try again later.');
+    return;
+  }
+
+  // Replace the corresponding text in the HTML file using ejs
+  const data = {
+    c_id: results.rows[0].id,
+    n_name1: results.rows[0].name,
+    n_name2: results.rows[0].name,
+    n_name3: results.rows[0].name,
+    n_description1: results.rows[0].description,
+    n_description2: results.rows[1].description,
+    n_description3: results.rows[2].description,
+    c_price: results.rows[0].price,
+    c_stock: results.rows[0].stock,
+    c_category: results.rows[0].category
+  };
+  const renderedHtml = ejs.render(html, data);
+  res.send(renderedHtml);
+});
 // app.use(express.static('.'));
 app.use(express.static(path.join(__dirname, '/')));
 
