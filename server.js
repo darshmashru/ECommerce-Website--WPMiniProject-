642789a9df3e1389cd34b467b3cf723df9c423b3
Darsh Mashru <mashrudarsh31@gmail.com>
2023-10-25 19:27:52 +0530

    Migration to MongoDB
    Updated for Tarun Anish Vedant AWP Mini Project

--- server.js
+++ server.js
@@ -1,131 +1,46 @@
-// import { connect } from '@planetscale/database'
-// import express from 'express';
-// import ejs from 'ejs';
-// import fs from 'fs/promises';
-
-// // Config for PlanetScale
-// const config = {
-//   host: process.env.PLANETSCALE_DB_HOST,
-//   username: process.env.PLANETSCALE_DB_USERNAME,
-//   password: process.env.PLANETSCALE_DB_PASSWORD
-// }
-
-// const conn = connect(config)
-
-// let html = ''; // Global variable to store the HTML contents
-// let results = null; // Global variable to store the database results
-
-// // Read the contents of the HTML file and execute database query when the server starts up
-// Promise.all([fs.readFile('candles.html', 'utf8'), conn.execute('SELECT * from merkaba.candles where id="1"')])
-//   .then(([file, res]) => {
-//     html = file;
-//     results = res;
-//   })
-//   .catch((err) => {
-//     console.error('Error occurred while initializing server:', err);
-//     process.exit(1);
-//   });
-
-// const app = express();
-
-// app.get('/candles.html', (req, res) => {
-//   if (results === null) {
-//     res.status(500).send('Internal server error. Please try again later.');
-//     return;
-//   }
-
-//   // Replace the corresponding text in the HTML file using ejs
-//   const data = {
-//     c_id: results.rows[0].id,
-//     c_name: results.rows[0].name,
-//     c_description: results.rows[0].description,
-//     c_price: results.rows[0].price,
-//     c_stock: results.rows[0].stock,
-//     c_category: results.rows[0].category
-//   };
-//   const renderedHtml = ejs.render(html, data);
-//   res.send(renderedHtml);
-// });
-
-// app.use(express.static('.'));
-
-// app.listen(3000, () => {
-//   console.log('Server started on port 3000');
-// });
-
-// export default app;
-
-
-// new logic
-
-import { connect } from '@planetscale/database'
 import express from 'express';
 import ejs from 'ejs';
 import fs from 'fs/promises';
-
+import { MongoClient } from 'mongodb';
 import { fileURLToPath } from 'url';
 import path from 'path';
+import dotenv from 'dotenv';
+import { ObjectId } from 'mongodb';
+dotenv.config();
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
+// Config for MongoDB
+const uri = process.env.MONGODB_URI;
+console.log(uri)
 
-// Config for PlanetScale
-const config = {
-  host: process.env.PLANETSCALE_DB_HOST,
-  username: process.env.PLANETSCALE_DB_USERNAME,
-  password: process.env.PLANETSCALE_DB_PASSWORD
-}
-
-const conn = connect(config)
+let client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let html = ''; // Global variable to store the HTML contents
 let results = null; // Global variable to store the database results
 
-// Read the contents of the HTML file and execute database query when the server starts up
-
-// Candles
-// Promise.all([fs.readFile(path.join(__dirname, 'candles.html'), 'utf8'), conn.execute('SELECT * from merkaba.candles where id="1"')])
-//   .then(([file, res]) => {
-//     html = file;
-//     results = res;
-//   })
-//   .catch((err) => {
-//     console.error('Error occurred while initializing server:', err);
-//     process.exit(1);
-//   });
-
-// Index
-Promise.all([fs.readFile(path.join(__dirname, 'index.html'), 'utf8'), conn.execute('SELECT * FROM merkaba.necklaces;')])
-  .then(([file, res]) => {
-    html = file;
-    results = res;
-  })
-  .catch((err) => {
-    console.error('Error occurred while initializing server:', err);
+// Connect to the MongoDB client
+client.connect(err => {
+  if (err) {
+    console.error('Error occurred while connecting to MongoDB:', err);
     process.exit(1);
-  });
-
-const app = express();
-
-
-app.get('/candles.html', (req, res) => {
-  if (results === null) {
-    res.status(500).send('Internal server error. Please try again later.');
-    return;
   }
 
-  // Replace the corresponding text in the HTML file using ejs
-  const data = {
-    c_id: results.rows[0].id,
-    c_name: results.rows[0].name,
-    c_description: results.rows[0].description,
-    c_price: results.rows[0].price,
-    c_stock: results.rows[0].stock,
-    c_category: results.rows[0].category
-  };
-  const renderedHtml = ejs.render(html, data);
-  res.send(renderedHtml);
+  const collection = client.db("Merkaba").collection("necklaces");
+
+  // Read the contents of the HTML file and execute database query when the server starts up
+  Promise.all([fs.readFile(path.join(__dirname, 'index.html'), 'utf8'), collection.find({}).toArray()])
+    .then(([file, res]) => {
+      html = file;
+      results = res;
+    })
+    .catch((err) => {
+      console.error('Error occurred while initializing server:', err);
+      process.exit(1);
+    });
 });
+
+const app = express();
 
 app.get('/', (req, res) => {
   if (results === null) {
@@ -135,28 +50,60 @@ app.get('/', (req, res) => {
 
   // Replace the corresponding text in the HTML file using ejs
   const data = {
-    c_id: results.rows[0].id,
-    n_name1: results.rows[0].name,
-    n_name2: results.rows[1].name,
-    n_name3: results.rows[2].name,
-    n_description1: results.rows[0].description,
-    n_description2: results.rows[1].description,
-    n_description3: results.rows[2].description,
-    c_price: results.rows[0].price,
-    c_stock: results.rows[0].stock,
-    c_category: results.rows[0].category
+    c_id: results[0]._id,
+    n_name1: results[0].name,
+    n_name2: results[1].name,
+    n_name3: results[2].name,
+    n_description1: results[0].description,
+    n_description2: results[1].description,
+    n_description3: results[2].description,
+    c_price: results[0].price,
+    c_stock: results[0].stock,
+    c_category: results[0].category
   };
   const renderedHtml = ejs.render(html, data);
   res.send(renderedHtml);
 });
 
+app.get('/index.html', async (req, res) => {
+  try {
+    const database = client.db("Merkaba");
+    const products = database.collection("necklaces");
+
+    // Get all documents in the collection.
+    const documents = await products.find().toArray();
+
+    if (!documents || documents.length === 0) {
+      console.log('No documents found');
+      res.status(404).send('No products found');
+      return;
+    }
+    console.log('Documents:', documents);
+
+    // Create a single data object for rendering.
+    const data = {};
+    documents.forEach((document, index) => {
+      data[`n_id${index + 1}`] = document._id;
+      data[`n_name${index + 1}`] = document.name;
+      data[`n_description${index + 1}`] = document.price;
+    });
+    console.log('Data:', data);
+
+    // Ensure that the html variable contains the correct HTML content
+    const html = await fs.readFile(path.join(__dirname, 'index.html'), 'utf8');
+    console.log('HTML:', html);
+
+    // Render the HTML with the data object.
+    const renderedHtml = ejs.render(html, data);
+    console.log('Rendered HTML:', renderedHtml);
+    res.send(renderedHtml);
+  } catch (err) {
+    console.error('Error occurred while fetching products:', err);
+    res.status(500).send('Internal server error. Please try again later.');
+  }
+});
+
 app.use(express.static(path.join(__dirname, './')));
-// app.use(express.static('.'));
-
-
-// app.listen(3000, () => {
-//   console.log('Server started on port 3000');
-// });
 
 const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
