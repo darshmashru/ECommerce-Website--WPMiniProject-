51595ec68aa726d46750c238adfd37777354e6ec
Tarun Mahajan <tarsunmah@gmail.com>
2023-10-25 21:20:58 +0530

    updated all pages with mongodb

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ import { MongoClient } from 'mongodb';
 import { fileURLToPath } from 'url';
 import path from 'path';
 import dotenv from 'dotenv';
-import { ObjectId } from 'mongodb';
 dotenv.config();
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // Config for MongoDB
 const uri = process.env.MONGODB_URI;
-console.log(uri)
+// const uri = process.dotenv.MONGODB_URI;
+// console.log(uri)
 
 let client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -25,6 +25,7 @@ client.connect(err => {
     console.error('Error occurred while connecting to MongoDB:', err);
     process.exit(1);
   }
+  console.log("Database Connected")
 
   const collection = client.db("Merkaba").collection("necklaces");
 
@@ -42,27 +43,40 @@ client.connect(err => {
 
 const app = express();
 
-app.get('/', (req, res) => {
-  if (results === null) {
-    res.status(500).send('Internal server error. Please try again later.');
-    return;
-  }
+app.get('/candles.html', async (req, res) => {
+  try {
+    const database = client.db("Merkaba");
+    const products = database.collection("candles");
 
-  // Replace the corresponding text in the HTML file using ejs
-  const data = {
-    c_id: results[0]._id,
-    n_name1: results[0].name,
-    n_name2: results[1].name,
-    n_name3: results[2].name,
-    n_description1: results[0].description,
-    n_description2: results[1].description,
-    n_description3: results[2].description,
-    c_price: results[0].price,
-    c_stock: results[0].stock,
-    c_category: results[0].category
-  };
-  const renderedHtml = ejs.render(html, data);
-  res.send(renderedHtml);
+    // Get all documents in the collection.
+    const documents = await products.find().toArray();
+
+    if (!documents || documents.length === 0) {
+      console.log('No documents found');
+      res.status(404).send('No products found');
+      return;
+    }
+
+    // Create a single data object for rendering.
+    const data = {};
+    console.log("Data:" + data);
+    documents.forEach((document, index) => {
+      data[`n_id${index + 1}`] = document._id;
+      data[`n_name${index + 1}`] = document.name;
+      data[`n_desc${index + 1}`] = document.description;
+      data[`n_price${index + 1}`] = document.price;
+    });
+
+    // Ensure that the html variable contains the correct HTML content
+    const html = await fs.readFile(path.join(__dirname, 'candles.html'), 'utf8');
+
+    // Render the HTML with the data object.
+    const renderedHtml = ejs.render(html, data);
+    res.send(renderedHtml);
+  } catch (err) {
+    console.error('Error occurred while fetching products:', err);
+    res.status(500).send('Internal server error. Please try again later.');
+  }
 });
 
 app.get('/index.html', async (req, res) => {
@@ -78,24 +92,122 @@ app.get('/index.html', async (req, res) => {
       res.status(404).send('No products found');
       return;
     }
-    console.log('Documents:', documents);
 
     // Create a single data object for rendering.
     const data = {};
     documents.forEach((document, index) => {
       data[`n_id${index + 1}`] = document._id;
       data[`n_name${index + 1}`] = document.name;
-      data[`n_description${index + 1}`] = document.price;
+      data[`n_price${index + 1}`] = document.price;
     });
-    console.log('Data:', data);
 
     // Ensure that the html variable contains the correct HTML content
     const html = await fs.readFile(path.join(__dirname, 'index.html'), 'utf8');
-    console.log('HTML:', html);
 
     // Render the HTML with the data object.
     const renderedHtml = ejs.render(html, data);
-    console.log('Rendered HTML:', renderedHtml);
+    res.send(renderedHtml);
+  } catch (err) {
+    console.error('Error occurred while fetching products:', err);
+    res.status(500).send('Internal server error. Please try again later.');
+  }
+});
+
+app.get('/necklaces.html', async (req, res) => {
+  try {
+    const database = client.db("Merkaba");
+    const products = database.collection("necklaces");
+
+    // Get all documents in the collection.
+    const documents = await products.find().toArray();
+
+    if (!documents || documents.length === 0) {
+      console.log('No documents found');
+      res.status(404).send('No products found');
+      return;
+    }
+
+    // Create a single data object for rendering.
+    const data = {};
+    documents.forEach((document, index) => {
+      data[`n_id${index + 1}`] = document._id;
+      data[`n_name${index + 1}`] = document.name;
+      data[`n_price${index + 1}`] = document.price;
+    });
+
+    // Ensure that the html variable contains the correct HTML content
+    const html = await fs.readFile(path.join(__dirname, 'necklaces.html'), 'utf8');
+
+    // Render the HTML with the data object.
+    const renderedHtml = ejs.render(html, data);
+    res.send(renderedHtml);
+  } catch (err) {
+    console.error('Error occurred while fetching products:', err);
+    res.status(500).send('Internal server error. Please try again later.');
+  }
+});
+
+app.get('/bracelets.html', async (req, res) => {
+  try {
+    const database = client.db("Merkaba");
+    const products = database.collection("bracelets");
+
+    // Get all documents in the collection.
+    const documents = await products.find().toArray();
+
+    if (!documents || documents.length === 0) {
+      console.log('No documents found');
+      res.status(404).send('No products found');
+      return;
+    }
+
+    // Create a single data object for rendering.
+    const data = {};
+    documents.forEach((document, index) => {
+      data[`n_id${index + 1}`] = document._id;
+      data[`n_name${index + 1}`] = document.name;
+      data[`n_price${index + 1}`] = document.price;
+    });
+
+    // Ensure that the html variable contains the correct HTML content
+    const html = await fs.readFile(path.join(__dirname, 'bracelets.html'), 'utf8');
+
+    // Render the HTML with the data object.
+    const renderedHtml = ejs.render(html, data);
+    res.send(renderedHtml);
+  } catch (err) {
+    console.error('Error occurred while fetching products:', err);
+    res.status(500).send('Internal server error. Please try again later.');
+  }
+});
+
+app.get('/rings.html', async (req, res) => {
+  try {
+    const database = client.db("Merkaba");
+    const products = database.collection("rings");
+
+    // Get all documents in the collection.
+    const documents = await products.find().toArray();
+
+    if (!documents || documents.length === 0) {
+      console.log('No documents found');
+      res.status(404).send('No products found');
+      return;
+    }
+
+    // Create a single data object for rendering.
+    const data = {};
+    documents.forEach((document, index) => {
+      data[`n_id${index + 1}`] = document._id;
+      data[`n_name${index + 1}`] = document.name;
+      data[`n_price${index + 1}`] = document.price;
+    });
+
+    // Ensure that the html variable contains the correct HTML content
+    const html = await fs.readFile(path.join(__dirname, 'rings.html'), 'utf8');
+
+    // Render the HTML with the data object.
+    const renderedHtml = ejs.render(html, data);
     res.send(renderedHtml);
   } catch (err) {
     console.error('Error occurred while fetching products:', err);
